3b79709afad47fb596fe4da3aa26fab130f5570c
Guilherme Azambuja <guilhermevazambuja@gmail.com>
2024-01-22 23:53:58 -0300

    main.py app entry

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+# https://github.com/gvlk/flask-crud
+# Guilherme Azambuja - 149429
+
 from app import setup_app
 
-app, db = setup_app()
+USERNAME = "postgres"
+PASSWORD = "root"
+HOST = "localhost"
+DATABASE = "flask_crud"
+
+app, db = setup_app(
+	USERNAME,
+	PASSWORD,
+	HOST,
+	DATABASE
+)
 
 if __name__ == "__main__":
 	app.run()
