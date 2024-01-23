3d9fe24b04ac159946f3b5435fceb37de2aa5d38
Guilherme Azambuja <guilhermevazambuja@gmail.com>
2024-01-22 23:54:17 -0300

    app views

--- app/views.py
+++ app/views.py
@@ -1,123 +1,152 @@
-from flask import Blueprint, render_template, redirect, url_for, request ,Response
+from datetime import datetime
+from random import choice, randint
+
+from flask import Blueprint, render_template, redirect, url_for, request, Response
 
 from . import db
 from .models import Song
-from random import choice, randint
-from datetime import datetime
-
 
 main_bp = Blueprint('main', __name__)
 
 
+class SampleData:
+	TITLES = (
+		"Song A", "Song B", "Song C", "Song D", "Song E",
+		"Song F", "Song G", "Song H", "Song I", "Song J"
+	)
+
+	ARTISTS = (
+		"Artist X", "Artist Y", "Artist Z", "Artist W", "Artist V",
+		"Artist U", "Artist T", "Artist S", "Artist R", "Artist Q"
+	)
+
+	ALBUMS = (
+		"Album 1", "Album 2", "Album 3", "Album 4", "Album 5",
+		"Album 6", "Album 7", "Album 8", "Album 9", "Album 10"
+	)
+
+	GENRES = (
+		"Pop", "Rock", "Hip Hop", "Country", "Electronic",
+		"R&B", "Jazz", "Blues", "Reggae", "Classical"
+	)
+
+
 @main_bp.route('/')
-def home() -> str:
+def home(**kwargs) -> str:
 	songs = Song.query.all()
-	return render_template("index.html", songs=songs)
+	return render_template("index.html", songs=songs, kwargs=kwargs)
 
 
-@main_bp.route('/populate', methods=['POST'])
-def populate_songs() -> Response:
-	if request.method == 'POST':
-		Song.query.delete()
-		db.session.commit()
+@main_bp.route('/insert', methods=['POST'])
+def insert_song() -> Response | str:
+	# Retrieve data from the form
+	(
+		title,
+		artist,
+		album,
+		duration_seconds,
+		genre,
+		release_date,
+	) = process_form_data()
 
-		titles = ("Song A", "Song B", "Song C")
-		artists = ("Artist X", "Artist Y", "Artist Z")
-		albums = ("Album 1", "Album 2", "Album 3")
-		genres = ("Pop", "Rock", "Hip Hop")
-		lyrics = "Sample lyrics"
+	# Create a new Song instance
+	new_song = Song(
+		title=title,
+		artist=artist,
+		album=album,
+		duration_seconds=duration_seconds,
+		genre=genre,
+		release_date=release_date,
+	)
 
-		for i in range(10):
-			title = choice(titles)
-			artist = choice(artists)
-			album = choice(albums)
-			duration_seconds = randint(180, 300)
-			genre = choice(genres)
-			release_date = "2022-01-01"
-
-			song = Song(
-				title=title,
-				artist=artist,
-				album=album,
-				duration_seconds=duration_seconds,
-				genre=genre,
-				release_date=release_date,
-				lyrics=lyrics
-			)
-			db.session.add(song)
-
-		db.session.commit()
-	return redirect(url_for('main.home'))
-
-
-@main_bp.route('/create', methods=['POST'])
-def create_song() -> Response | str:
-	if request.method == 'POST':
-		# Retrieve data from the form
-		title = request.form.get('title')
-		artist = request.form.get('artist')
-		album = request.form.get('album')
-		duration_seconds = request.form.get('duration_seconds')
-		genre = request.form.get('genre')
-		release_date = request.form.get('release_date')
-		lyrics = request.form.get('lyrics')
-
-		duration_seconds = int(duration_seconds) if duration_seconds else 0
-		release_date = release_date if release_date else datetime.now()
-
-		# Create a new Song instance
-		new_song = Song(
-			title=title,
-			artist=artist,
-			album=album,
-			duration_seconds=duration_seconds,
-			genre=genre,
-			release_date=release_date,
-			lyrics=lyrics
-		)
-
-		# Add the new song to the database
-		db.session.add(new_song)
-		db.session.commit()
+	# Add the new song to the database
+	db.session.add(new_song)
+	db.session.commit()
 
 	return redirect(url_for('main.home'))
 
 
 @main_bp.route('/update/<int:song_id>', methods=['POST'])
-def update_song(song_id: int) -> Response:
-	if request.method == 'POST':
-		# Retrieve data from the form
-		title = request.form.get('title')
-		artist = request.form.get('artist')
-		album = request.form.get('album')
-		duration_seconds = int(request.form.get('duration_seconds'))
-		genre = request.form.get('genre')
-		release_date = request.form.get('release_date')
-		lyrics = request.form.get('lyrics')
+def update_song(song_id: int) -> Response | str:
+	(
+		title,
+		artist,
+		album,
+		duration_seconds,
+		genre,
+		release_date,
+	) = process_form_data()
 
-		# Find the song to update by its ID
-		song_to_update = Song.query.get(song_id)
+	# Find the song to update by its ID
+	song_to_update = Song.query.get(song_id)
 
-		# Update the song attributes
-		song_to_update.title = title
-		song_to_update.artist = artist
-		song_to_update.album = album
-		song_to_update.duration_seconds = duration_seconds
-		song_to_update.genre = genre
-		song_to_update.release_date = release_date
-		song_to_update.lyrics = lyrics
+	# Update the song attributes
+	song_to_update.title = title
+	song_to_update.artist = artist
+	song_to_update.album = album
+	song_to_update.duration_seconds = duration_seconds
+	song_to_update.genre = genre
+	song_to_update.release_date = release_date
 
-		# Commit the changes to the database
-		db.session.commit()
+	# Commit the changes to the database
+	db.session.commit()
 
 	return redirect(url_for('main.home'))
 
 
-@main_bp.route('/delete/<int:song_id>', methods=['POST'])
-def delete_song(song_id: int) -> Response:
+@main_bp.route('/remove/<int:song_id>', methods=['POST'])
+def remove_song(song_id: int) -> Response:
 	song_to_delete = Song.query.get(song_id)
 
 	db.session.delete(song_to_delete)
 	db.session.commit()
 
 	return redirect(url_for('main.home'))
+
+
+@main_bp.route('/populate', methods=['POST'])
+def populate_songs() -> Response:
+	db.session.commit()
+
+	for i in range(10):
+		title = choice(SampleData.TITLES)
+		artist = choice(SampleData.ARTISTS)
+		album = choice(SampleData.ALBUMS)
+		duration_seconds = randint(120, 300)
+		genre = choice(SampleData.GENRES)
+		release_date = f"{randint(1900, 2030)}-{randint(1, 12)}-{randint(1, 28)}"
+
+		song = Song(
+			title=title,
+			artist=artist,
+			album=album,
+			duration_seconds=duration_seconds,
+			genre=genre,
+			release_date=release_date,
+		)
+		db.session.add(song)
+
+	db.session.commit()
+	return redirect(url_for('main.home'))
+
+
+@main_bp.route('/clear', methods=['POST'])
+def clear_table() -> Response:
+	Song.query.delete()
+	db.session.commit()
+
+	return redirect(url_for('main.home'))
+
+
+def process_form_data() -> tuple:
+	title = request.form.get('title')
+	artist = request.form.get('artist')
+	album = request.form.get('album')
+	raw_duration_seconds = request.form.get('duration_seconds')
+	genre = request.form.get('genre')
+	raw_release_date = request.form.get('release_date')
+
+	duration_seconds = int(raw_duration_seconds) if raw_duration_seconds else 0
+	release_date = raw_release_date if raw_release_date else datetime.now()
+
+	return title, artist, album, duration_seconds, genre, release_date
